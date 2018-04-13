312dc1962711d7421f1dd456b6765f3883b6877d
Bob van de Vijver <bob@drenso.nl>
2018-04-13 11:48:37 +0200

    Added study area switcher at dashboard

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\StudyArea;
 use App\Repository\StudyAreaRepository;
 use App\Request\Wrapper\RequestStudyArea;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
@@ -15,7 +18,7 @@ class DefaultController extends Controller
 
   /**
    * @Route("/", defaults={"_studyArea"=null, "pageUrl"=""})
-   * @Route("/page/{_studyArea}/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"_studyArea"="\d+", "pageUrl"=".+"}, name="_home")
+   * @Route("/page/{_studyArea}/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"_studyArea"="\d+", "pageUrl"=".+"}, name="_home", options={"expose"=true})
    * @Route("/page/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"pageUrl"=".+"})
    * @Template("double_column.html.twig")
    *
@@ -52,8 +55,34 @@ class DefaultController extends Controller
    */
   public function dashboard(RequestStudyArea $requestStudyArea)
   {
+    // @todo Check for amount of available study areas
+    if (true) {
+      $form = $this->createFormBuilder()
+          ->add('studyArea', EntityType::class, [
+              'label'         => 'dashboard.study-area-switch',
+              'class'         => StudyArea::class,
+              'select2'       => true,
+              'query_builder' => function (StudyAreaRepository $studyAreaRepository) use ($requestStudyArea) {
+                return $studyAreaRepository->createQueryBuilder('sa')
+                    ->where('sa != :current')
+                    ->setParameter('current', $requestStudyArea->getStudyArea())
+                    ->orderBy('sa.name');
+              },
+          ])
+          ->add('submit', SubmitType::class, [
+              'label' => 'study-area.switch-to',
+              'icon'  => 'fa-chevron-right',
+              'attr'  => array(
+                  'class'   => 'btn btn-outline-success',
+                  'onclick' => 'eDispatch.pageLoad(Routing.generate(\'_home\', {\'_studyArea\': $(\'#form_studyArea\').val()}), {topLevel: true}); return false;',
+              ),
+          ])
+          ->getForm();
+    }
+
     return [
-        'studyArea' => $requestStudyArea->getStudyArea()
+        'form'      => isset($form) ? $form->createView() : NULL,
+        'studyArea' => $requestStudyArea->getStudyArea(),
     ];
   }
 }
