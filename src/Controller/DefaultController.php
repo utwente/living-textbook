bbe94a6d62f2731f4feeac6433f2f48af6eca613
Bob van de Vijver <bob@drenso.nl>
2018-08-16 21:52:00 +0200

    Implemented landing page after logon

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Concept;
 use App\Entity\StudyArea;
+use App\Entity\User;
 use App\Repository\AbbreviationRepository;
 use App\Repository\ConceptRepository;
 use App\Repository\ExternalResourceRepository;
@@ -16,17 +17,19 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 
 class DefaultController extends Controller
 {
-
   /**
-   * @Route("/", defaults={"_studyArea"=null, "pageUrl"=""})
-   * @Route("/page/{_studyArea}/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"_studyArea"="\d+", "pageUrl"=".+"}, name="_home", options={"expose"=true})
-   * @Route("/page/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"pageUrl"=".+"}, name="_home_simple")
+   * @Route("/page/{_studyArea}/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""},
+   *                                        requirements={"_studyArea"="\d+", "pageUrl"=".+"}, name="_home",
+   *                                        options={"expose"=true})
+   * @Route("/page/{pageUrl}", defaults={"_studyArea"=null, "pageUrl"=""}, requirements={"pageUrl"=".+"},
+   *                           name="_home_simple")
    * @Template("double_column.html.twig")
    * @IsGranted("ROLE_USER")
    *
@@ -59,12 +62,36 @@ class DefaultController extends Controller
   }
 
   /**
+   * @Route("/")
+   * @Template()
+   * @IsGranted("ROLE_USER")
+   *
+   * @param FormFactoryInterface $formFactory
+   * @param StudyAreaRepository  $studyAreaRepository
+   *
+   * @return array
+   */
+  public function landing(FormFactoryInterface $formFactory, StudyAreaRepository $studyAreaRepository)
+  {
+    $user       = $this->getUser();
+    $studyAreas = $studyAreaRepository->getVisible($this->getUser());
+
+    // Only show select form when there is more than 1 visible study area
+    $studyAreaForm = count($studyAreas) > 1 ? $this->createStudyAreaForm($formFactory, $user, NULL, 'dashboard.open') : NULL;
+
+    return [
+        'studyAreaForm' => $studyAreaForm ? $studyAreaForm->createView() : NULL,
+    ];
+  }
+
+  /**
    * @Route("/{_studyArea}/dashboard", requirements={"_studyArea"="\d+"})
    * @Route("/{_studyArea}/dashboard", requirements={"_studyArea"="\d+"}, name="app_studyarea_list")
    * @Template
    * @IsGranted("STUDYAREA_SHOW", subject="requestStudyArea")
    *
    * @param RequestStudyArea           $requestStudyArea
+   * @param FormFactoryInterface       $formFactory
    * @param StudyAreaRepository        $studyAreaRepository
    * @param ConceptRepository          $conceptRepo
    * @param AbbreviationRepository     $abbreviationRepository
@@ -82,6 +109,9 @@ class DefaultController extends Controller
     $user       = $this->getUser();
     $studyArea  = $requestStudyArea->getStudyArea();
     $studyAreas = $studyAreaRepository->getVisible($this->getUser());
+
+    // Only show switch form when there is more than 1 visible study area
+    $studyAreaForm = count($studyAreas) > 1 ? $this->createStudyAreaForm($formFactory, $user, $studyArea) : NULL;
 
     $conceptForm = $formFactory->createNamedBuilder('concept_form')
         ->add('concept', EntityType::class, [
@@ -104,35 +134,9 @@ class DefaultController extends Controller
         ])
         ->getForm();
 
-    // Only show switch form when there is more than 1 visible study area
-    if (count($studyAreas) > 1) {
-      $studyAreaForm = $formFactory->createNamedBuilder('studyarea_form')
-          ->add('studyArea', EntityType::class, [
-              'placeholder'   => 'dashboard.select-one',
-              'hide_label'    => true,
-              'class'         => StudyArea::class,
-              'select2'       => true,
-              'query_builder' => function (StudyAreaRepository $studyAreaRepository) use ($user, $studyArea) {
-                return $studyAreaRepository->getVisibleQueryBuilder($user)
-                    ->andWhere('sa != :current')
-                    ->setParameter('current', $studyArea)
-                    ->orderBy('sa.name');
-              },
-          ])
-          ->add('submit', SubmitType::class, [
-              'disabled' => true,
-              'label'    => 'study-area.switch-to',
-              'icon'     => 'fa-chevron-right',
-              'attr'     => array(
-                  'class' => 'btn btn-outline-success',
-              ),
-          ])
-          ->getForm();
-    }
-
     return [
         'conceptForm'           => $conceptForm->createView(),
-        'studyAreaForm'         => isset($studyAreaForm) ? $studyAreaForm->createView() : NULL,
+        'studyAreaForm'         => $studyAreaForm ? $studyAreaForm->createView() : NULL,
         'studyArea'             => $studyArea,
         'studyAreas'            => $studyAreas,
         'currentStudyArea'      => $requestStudyArea->getStudyArea(),
@@ -142,4 +146,43 @@ class DefaultController extends Controller
         'learningOutcomeCount'  => $learningOutcomeRepo->getCountForStudyArea($studyArea),
     ];
   }
+
+  /**
+   * @param FormFactoryInterface $formFactory
+   * @param User                 $user
+   * @param StudyArea|null       $studyArea
+   * @param string               $buttonLabel
+   *
+   * @return \Symfony\Component\Form\FormInterface
+   */
+  private function createStudyAreaForm(FormFactoryInterface $formFactory, User $user, ?StudyArea $studyArea,
+                                       string $buttonLabel = 'study-area.switch-to'): FormInterface
+  {
+    return $formFactory->createNamedBuilder('studyarea_form')
+        ->add('studyArea', EntityType::class, [
+            'placeholder'   => 'dashboard.select-one',
+            'hide_label'    => true,
+            'class'         => StudyArea::class,
+            'select2'       => true,
+            'query_builder' => function (StudyAreaRepository $studyAreaRepository) use ($user, $studyArea) {
+              $qb = $studyAreaRepository->getVisibleQueryBuilder($user);
+              if ($studyArea !== NULL) {
+                $qb->andWhere('sa != :current')
+                    ->setParameter('current', $studyArea);
+              }
+
+              return $qb->orderBy('sa.name');
+            },
+        ])
+        ->add('submit', SubmitType::class, [
+            'disabled' => true,
+            'label'    => $buttonLabel,
+            'icon'     => 'fa-chevron-right',
+            'attr'     => array(
+                'class' => 'btn btn-outline-success',
+            ),
+        ])
+        ->getForm();
+  }
+
 }
