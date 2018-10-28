445d9142fc6eec4eea603d9a9d5638eb95db92db
Bob van de Vijver <bob@drenso.nl>
2018-10-28 10:39:07 +0100

    #111: No longer let the browser handle iframe posts, as it messes with the history stack

--- assets/js/content.js
+++ assets/js/content.js
@@ -70,20 +70,82 @@ $(function () {
     }
   });
 
+  // Forms on the page to parse
+  let $loadedForms = $('form');
+
+  // Send handler to form buttons to determine clicked button
+  let $submitButtons = $loadedForms.find(':input[type=submit]');
+  $submitButtons.click(function () {
+    $submitButtons.removeAttr('clicked');
+    $(this).attr('clicked', 'true');
+  });
+
   // Disable submit buttons on submit
-  let $form = $('form');
-  $form.submit(function () {
-    if ($(this).attr('target') === '_blank') return;
-    $(this).find(':input[type=submit]')
+  $loadedForms.submit(function () {
+    let $form = $(this);
+    if ($form.attr('target') === '_blank') return;
+
+    // Find buttons and disable them by binding a new handler
+    // If this is not done, the default submit action wouldn't be executed
+    $form.find(':input[type=submit]')
         .addClass('disabled')
         .on('click', function (e) {
           e.preventDefault();
           return false;
         });
+
+    let method = $form.attr('method').toUpperCase();
+    if (method === undefined) {
+      method = 'POST';
+    }
+
+    if (inDoubleColumn && method === 'POST') {
+      // Make sure we have the updated data from ckeditor
+      for (let instanceName in CKEDITOR.instances) {
+        if (!CKEDITOR.instances.hasOwnProperty(instanceName)) continue;
+        CKEDITOR.instances[instanceName].updateElement();
+      }
+
+      // Retrieve action
+      let action = $form.attr('action');
+      if (action === undefined) {
+        action = window.location.href;
+      }
+
+      // Serialize form data
+      let formData = $form.serialize();
+
+      // Retrieve clicked button data
+      formData += '&' + encodeURIComponent($(':input[type=submit][clicked=true]').attr('name')) + '=';
+
+      // Send actual POST request
+      $.ajax({
+        type: method,
+        url: action,
+        data: formData
+      }).always(function (data) {
+        // Before loading the new data, make sure to unload ckeditor instances
+        for (let instanceName in CKEDITOR.instances) {
+          if (!CKEDITOR.instances.hasOwnProperty(instanceName)) continue;
+          CKEDITOR.instances[instanceName].destroy();
+        }
+        // noinspection JSUndeclaredVariable
+        CKEDITOR = undefined;
+
+        // Replace content
+        document.open();
+        document.write(data);
+        document.close();
+      });
+
+      return false;
+    }
+
+    return true;
   });
 
   // Auto focus first form field
-  $form.find('input').first().focus();
+  $loadedForms.find('input').first().focus();
 
   // Page loaded event
   eDispatch.pageLoaded();
