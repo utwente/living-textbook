8e86bc6812565feb3189a4e064fa29a19e726be6
Tobias Feijten <tobias@drenso.nl>
2019-02-03 15:11:02 +0100

    Update dependencies part 1
    Package operations: 1 install, 15 updates, 0 removals
      - Updating studio-42/elfinder (2.1.43 => 2.1.46): Loading from cache
      - Updating twig/twig (v2.5.0 => v2.6.2): Loading from cache
      - Updating symfony/twig-bridge (v4.2.2 => v4.2.3): Loading from cache
      - Updating helios-ag/fm-elfinder-bundle (9.1.1 => 9.2): Loading from cache
      - Updating jms/serializer-bundle (2.4.2 => 2.4.3): Loading from cache
      - Installing markbaker/matrix (1.1.4): Loading from cache
      - Updating phpoffice/phpspreadsheet (1.5.2 => 1.6.0): Loading from cache
      - Updating phpseclib/phpseclib (2.0.12 => 2.0.14): Loading from cache
      - Updating sensio/framework-extra-bundle (v5.2.2 => v5.2.4): Loading from cache
      - Updating composer/ca-bundle (1.1.3 => 1.1.4): Loading from cache
      - Updating sensiolabs/security-checker (v4.1.8 => v5.0.3): Loading from cache
      - Updating drenso/phan-extensions (v2.4.1 => v2.4.2): Loading from cache
      - Updating doctrine/dbal (v2.9.0 => v2.9.2): Loading from cache
      - Updating doctrine/migrations (v1.8.1 => v2.0.0): Loading from cache
      - Updating doctrine/doctrine-bundle (1.10.0 => 1.10.1): Loading from cache
      - Updating doctrine/doctrine-migrations-bundle (v1.3.2 => v2.0.0): Loading from cache

--- src/Migrations/2018/05/Version20180511091759.php
+++ src/Migrations/2018/05/Version20180511091759.php
@@ -2,7 +2,7 @@
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -10,7 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 final class Version20180511091759 extends AbstractMigration
 {
-  public function up(Schema $schema)
+  public function up(Schema $schema): void
   {
     // this up() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -20,7 +20,7 @@ final class Version20180511091759 extends AbstractMigration
     $this->addSql('ALTER TABLE study_area ADD CONSTRAINT FK_A84CD5512B18554A FOREIGN KEY (owner_user_id) REFERENCES user__table (id)');
   }
 
-  public function down(Schema $schema)
+  public function down(Schema $schema): void
   {
     // this down() migration is auto-generated, please modify it to your needs
     $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
