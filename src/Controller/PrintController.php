d3124f0a957cd7d29a1ce7325a137690e6279b5a
Bob van de Vijver <bob@drenso.nl>
2019-07-31 17:44:03 +0200

    #227: Catch image not found exceptions

--- src/Controller/PrintController.php
+++ src/Controller/PrintController.php
@@ -7,7 +7,9 @@ use App\ConceptPrint\Section\ConceptSection;
 use App\ConceptPrint\Section\LearningPathSection;
 use App\Entity\Concept;
 use App\Entity\LearningPath;
+use App\Entity\StudyArea;
 use App\Request\Wrapper\RequestStudyArea;
+use BobV\LatexBundle\Exception\ImageNotFoundException;
 use BobV\LatexBundle\Generator\LatexGeneratorInterface;
 use BobV\LatexBundle\Helper\Sanitize;
 use Exception;
@@ -41,8 +43,9 @@ class PrintController extends AbstractController
    * @return Response
    * @throws Exception
    */
-  public function printSingleConcept(RequestStudyArea $requestStudyArea, Concept $concept, LatexGeneratorInterface $generator,
-                                     TranslatorInterface $translator, RouterInterface $router)
+  public function printSingleConcept(
+      RequestStudyArea $requestStudyArea, Concept $concept, LatexGeneratorInterface $generator,
+      TranslatorInterface $translator, RouterInterface $router)
   {
     // Check if correct study area
     if ($concept->getStudyArea()->getId() != $requestStudyArea->getStudyArea()->getId()) {
@@ -60,7 +63,11 @@ class PrintController extends AbstractController
         ->addElement(new ConceptSection($concept, $router, $translator, $projectDir));
 
     // Return PDF
-    return $generator->createPdfResponse($document, false);
+    try {
+      return $generator->createPdfResponse($document, false);
+    } catch (Exception $e) {
+      return $this->parsePrintException($e, $concept);
+    }
   }
 
   /**
@@ -96,11 +103,71 @@ class PrintController extends AbstractController
         ->addElement(new LearningPathSection($learningPath, $router, $translator, $projectDir));
 
     // Return PDF
-    return $generator->createPdfResponse($document, false);
+    try {
+      return $generator->createPdfResponse($document, false);
+    } catch (Exception $e) {
+      return $this->parsePrintException($e, NULL, $learningPath);
+    }
+
   }
 
   private function filename(string $name)
   {
     return str_replace(' ', '-', mb_strtolower(Sanitize::sanitizeText($name)));
+  }
+
+  /**
+   * Tries to parse the thrown exception into something that might be usable for the user.
+   *
+   * @param Exception         $e
+   *
+   * @param Concept|null      $concept
+   * @param LearningPath|null $learningPath
+   *
+   * @return Response
+   * @throws Exception
+   */
+  private function parsePrintException(Exception $e, Concept $concept = NULL, LearningPath $learningPath = NULL)
+  {
+    // Retrieve study area from one of the given objects
+    $studyArea = $concept ? $concept->getStudyArea() : NULL;
+    $studyArea = $studyArea ?? ($learningPath ? $learningPath->getStudyArea() : NULL);
+
+    switch (true) {
+      case $e instanceof ImageNotFoundException:
+        assert($e instanceof ImageNotFoundException);
+        $imageLocation = $e->getImageLocation();
+        $isUrl         = $this->isUrl($imageLocation);
+
+        return $this->render('print/image_not_found.html.twig', [
+            'isUrl'        => $isUrl,
+            'path'         => $isUrl ? $imageLocation : NULL,
+            'basename'     => $isUrl ? NULL : $this->getProjectPath($imageLocation, $studyArea),
+            'concept'      => $concept,
+            'learningPath' => $learningPath,
+            'studyArea'    => $studyArea,
+        ]);
+      default:
+        throw $e;
+        break;
+    }
+  }
+
+  private function isUrl(string $url): bool
+  {
+    return 1 === preg_match('@^https?://@', $url);
+  }
+
+  private function getProjectPath(string $url, StudyArea $studyArea)
+  {
+    // Try to match against expected project path
+    $matches = [];
+    $regex   = sprintf('@^%s/public/uploads/studyarea/%d/(.+)@', $this->getParameter('kernel.project_dir'), $studyArea->getId());
+    if (1 === preg_match($regex, $url, $matches)) {
+      return $matches[1];
+    }
+
+    // Just return the base url
+    return basename($url);
   }
 }
