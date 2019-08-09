4bd2af903b0b5d29480581164d4b8a7aa2cbf33f
Bob van de Vijver <bob@drenso.nl>
2019-08-09 12:11:00 +0200

    #229: Allow clearable select2 widgets

--- src/Form/Extension/Select2Extension.php
+++ src/Form/Extension/Select2Extension.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -22,12 +23,14 @@ class Select2Extension extends AbstractTypeExtension
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder->setAttribute('select2', $options['select2']);
+    $builder->setAttribute('select2_allow_clear', $options['select2_allow_clear']);
     $builder->setAttribute('select2_placeholder', $options['select2_placeholder']);
   }
 
   public function buildView(FormView $view, FormInterface $form, array $options)
   {
     $view->vars['select2']             = $options['select2'];
+    $view->vars['select2_allow_clear'] = $options['select2_allow_clear'];
     $view->vars['select2_placeholder'] = $options['select2_placeholder'];
   }
 
@@ -35,11 +38,25 @@ class Select2Extension extends AbstractTypeExtension
   {
     $resolver->setDefaults([
         'select2'             => false,
+        'select2_allow_clear' => NULL,
         'select2_placeholder' => NULL,
     ]);
 
     $resolver->setAllowedTypes('select2', ['bool']);
+    $resolver->setAllowedTypes('select2_allow_clear', ['null', 'bool']);
     $resolver->setAllowedTypes('select2_placeholder', ['null', 'string']);
+
+    $resolver->setNormalizer('select2_allow_clear', function (Options $options, $value) {
+      if ($value !== NULL) {
+        return $value;
+      }
+
+      if (!$options->offsetExists('required')) {
+        return $value;
+      }
+
+      return !$options->offsetGet('required');
+    });
   }
 
   public static function getExtendedTypes(): iterable
