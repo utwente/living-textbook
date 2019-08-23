bb8ec167e800ca2862113bbd32788db09c009f80
Bob van de Vijver <bob@drenso.nl>
2019-08-23 18:05:19 +0200

    #232: Added tracking events to tracking data export

--- src/Excel/TrackingExportBuilder.php
+++ src/Excel/TrackingExportBuilder.php
@@ -2,16 +2,19 @@
 
 namespace App\Excel;
 
+use App\Entity\PageLoad;
 use App\Entity\StudyArea;
+use App\Entity\TrackingEvent;
 use App\Repository\PageLoadRepository;
+use App\Repository\TrackingEventRepository;
 use PhpOffice\PhpSpreadsheet\Exception;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TrackingExportBuilder
 {
-
   /** @var TranslatorInterface */
   private $translator;
 
@@ -21,18 +24,25 @@ class TrackingExportBuilder
   /** @var PageLoadRepository */
   private $pageLoadRepository;
 
+  /** @var TrackingEventRepository */
+  private $trackingEventRepository;
+
   /**
    * TrackingExportBuilder constructor.
    *
-   * @param TranslatorInterface $translator
-   * @param SpreadsheetHelper   $spreadsheetHelper
-   * @param PageLoadRepository  $pageLoadRepository
+   * @param TranslatorInterface     $translator
+   * @param SpreadsheetHelper       $spreadsheetHelper
+   * @param PageLoadRepository      $pageLoadRepository
+   * @param TrackingEventRepository $trackingEventRepository
    */
-  public function __construct(TranslatorInterface $translator, SpreadsheetHelper $spreadsheetHelper, PageLoadRepository $pageLoadRepository)
+  public function __construct(
+      TranslatorInterface $translator, SpreadsheetHelper $spreadsheetHelper, PageLoadRepository $pageLoadRepository,
+      TrackingEventRepository $trackingEventRepository)
   {
-    $this->translator         = $translator;
-    $this->spreadsheetHelper  = $spreadsheetHelper;
-    $this->pageLoadRepository = $pageLoadRepository;
+    $this->translator              = $translator;
+    $this->spreadsheetHelper       = $spreadsheetHelper;
+    $this->pageLoadRepository      = $pageLoadRepository;
+    $this->trackingEventRepository = $trackingEventRepository;
   }
 
   /**
@@ -52,20 +62,34 @@ class TrackingExportBuilder
         ->setSubject($this->translator->trans('tracking.export.subject', ['%item%' => $studyArea->getName()]))
         ->setDescription($this->translator->trans('tracking.export.description', ['%item%' => $studyArea->getName()]));
 
+    // Add the exports
+
+    $this->exportPageLoads($studyArea, $spreadsheet->setActiveSheetIndex(0));
+    $spreadsheet->createSheet(1);
+    $this->exportEvents($studyArea, $spreadsheet->setActiveSheetIndex(1));
+    $spreadsheet->setActiveSheetIndex(0);
+
+    // Create response
+    return $this->spreadsheetHelper->createExcelResponse($spreadsheet,
+        sprintf('%s_tracking_export.xlsx', $studyArea->getName()));
+  }
+
+  /**
+   * @param StudyArea $studyArea
+   * @param Worksheet $sheet
+   */
+  private function exportPageLoads(StudyArea $studyArea, Worksheet $sheet): void
+  {
     // Location
-    $sheet  = $spreadsheet->getActiveSheet();
     $column = 0;
     $row    = 1;
 
     // Create header
-    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
-    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.user-id', true);
-    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
-    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.session-id', true);
-    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
-    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.timestamp', true);
+    $this->setCommonHeader($sheet, $column, $row, 'Page loads');
+
     $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
     $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.origin', true);
+
     $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
     $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.path', true);
 
@@ -102,34 +126,132 @@ class TrackingExportBuilder
       $row++;
       $column = 0;
 
-      $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $pageload->getUserId());
-      $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $pageload->getSessionId());
-      $this->spreadsheetHelper->setCellDateTime($sheet, ++$column, $row, $pageload->getTimestamp(), true);
+      // Set common header values
+      $this->setCommonHeaderValues($sheet, $column, $row, $pageload);
+
+      // Set extra header values
       $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $pageload->getOrigin());
       $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $pageload->getPath());
 
       // Map context items to spreadsheet
-      foreach ($pageload->getPathContext() as $cKey => $cItem) {
-        $cKey = strtolower($cKey);
+      $this->mapContextElements($sheet, $column, $row, $pageload->getPathContext(), $contextMap);
+    }
+  }
 
-        if (!array_key_exists($cKey, $contextMap)) {
-          // Create column
-          $contextMap[$cKey] = $contextMap['__next'];
-          $contextMap['__next']++;
 
-          $sheet->getColumnDimensionByColumn($contextMap[$cKey])->setAutoSize(true);
-          $this->spreadsheetHelper->setCellValue($sheet, $contextMap[$cKey], 1, $cKey, true);
-        }
+  /**
+   * @param StudyArea $studyArea
+   * @param Worksheet $sheet
+   */
+  private function exportEvents(StudyArea $studyArea, Worksheet $sheet): void
+  {
+    // Location
+    $column = 0;
+    $row    = 1;
 
-        $column = $contextMap[$cKey];
-        if ($column) {
-          $this->spreadsheetHelper->setCellValue($sheet, $column, $row, $cItem);
-        }
+    // Create header
+    $this->setCommonHeader($sheet, $column, $row, 'Events');
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.event', true);
+
+    // Context key map, predefine commonly used values
+    $contextMap = [];
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.concept', true);
+    $contextMap['conceptid'] = $column;
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.learning-path', true);
+    $contextMap['learningpathid'] = $column;
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.link', true);
+    $contextMap['link'] = $column;
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.link-blank', true);
+    $contextMap['blank'] = $column;
+
+    $contextMap['__next'] = ++$column;
+
+    // Create content
+    $trackingEvents = $this->trackingEventRepository->getByStudyAreaOrderedOnIds($studyArea);
+    foreach ($trackingEvents as $trackingEvent) {
+      $row++;
+      $column = 0;
+
+      // Set common header values
+      $this->setCommonHeaderValues($sheet, $column, $row, $trackingEvent);
+
+      // Set extra header values
+      $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $trackingEvent->getEvent());
+
+      // Map context items to spreadsheet
+      $this->mapContextElements($sheet, $column, $row, $trackingEvent->getContext(), $contextMap);
+    }
+  }
+
+  /**
+   * @param Worksheet            $sheet
+   * @param iterable             $context
+   * @param array                $contextMap
+   * @param int                  $row
+   * @param                      $column
+   */
+  private function mapContextElements(Worksheet &$sheet, int &$column, int $row, iterable $context, array $contextMap): void
+  {
+    foreach ($context as $cKey => $cItem) {
+      $cKey = strtolower($cKey);
+
+      if (!array_key_exists($cKey, $contextMap)) {
+        // Create column
+        $contextMap[$cKey] = $contextMap['__next'];
+        $contextMap['__next']++;
+
+        $sheet->getColumnDimensionByColumn($contextMap[$cKey])->setAutoSize(true);
+        $this->spreadsheetHelper->setCellValue($sheet, $contextMap[$cKey], 1, $cKey, true);
+      }
+
+      $column = $contextMap[$cKey];
+      if ($column) {
+        $this->spreadsheetHelper->setCellValue($sheet, $column, $row, $cItem);
       }
     }
-
-    // Create response
-    return $this->spreadsheetHelper->createExcelResponse($spreadsheet,
-        sprintf('%s_tracking_export.xlsx', $studyArea->getName()));
   }
+
+  /**
+   * @param Worksheet $sheet
+   * @param int       $column
+   * @param int       $row
+   * @param string    $title
+   */
+  private function setCommonHeader(Worksheet &$sheet, int &$column, int $row, string $title): void
+  {
+    $sheet->setTitle($title);
+
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.user-id', true);
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.session-id', true);
+    $sheet->getColumnDimensionByColumn(++$column)->setAutoSize(true);
+    $this->spreadsheetHelper->setCellTranslatedValue($sheet, $column, $row, 'tracking.export.timestamp', true);
+  }
+
+  /**
+   * @param Worksheet $sheet
+   * @param int       $column
+   * @param int       $row
+   * @param object    $object
+   */
+  private function setCommonHeaderValues(Worksheet &$sheet, int &$column, int $row, $object): void
+  {
+    assert($object instanceof PageLoad || $object instanceof TrackingEvent);
+
+    $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $object->getUserId());
+    $this->spreadsheetHelper->setCellValue($sheet, ++$column, $row, $object->getSessionId());
+    $this->spreadsheetHelper->setCellDateTime($sheet, ++$column, $row, $object->getTimestamp(), true);
+  }
+
 }
