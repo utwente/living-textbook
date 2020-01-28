3b74a2fc7ea70af0722db1fbca7c434be4e7b80c
Bob van de Vijver <bob@drenso.nl>
2020-01-28 16:52:53 +0100

    #182: Show diff information for text fields

---
+++ src/Twig/HtmlDiffExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Twig;
+
+use Caxy\HtmlDiff\HtmlDiff;
+use Caxy\HtmlDiff\HtmlDiffConfig;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class HtmlDiffExtension extends AbstractExtension
+{
+  /**
+   * Register functions
+   *
+   * @return array|TwigFilter[]
+   */
+  public function getFunctions()
+  {
+    return [
+        new TwigFunction('htmldiff', [$this, 'htmlDiff'], ['is_safe' => ['html']]),
+    ];
+  }
+
+  public function htmlDiff(string $a, string $b): string
+  {
+    $config = (new HtmlDiffConfig())
+        ->setPurifierEnabled(false);
+
+    return (HtmlDiff::create($a, $b, $config))->build();
+  }
+}
