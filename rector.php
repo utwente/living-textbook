3be1a01bedeb871baaa80148c2f6b4294263ec1f
Bob van de Vijver <bob@drenso.nl>
2022-04-30 14:44:58 +0200

    Update to latest Rector, and rework configuration

--- rector.php
+++ rector.php
@@ -2,18 +2,15 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-  // Get parameters
-  $parameters = $containerConfigurator->parameters();
-  $parameters
-      ->set(Option::PATHS, [__DIR__ . '/src'])
-      ->set(Option::AUTO_IMPORT_NAMES, true)
-      ->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81)
-      ->set(Option::SKIP, [
-        __DIR__ . '/src/Database/Traits/IdTrait.php', // @todo: Remove this when moving to attributes for Doctrine
-      ]);
+return static function (RectorConfig $rc): void {
+  $rc->paths([__DIR__ . '/src']);
+  $rc->importNames();
+  $rc->phpVersion(PhpVersion::PHP_81);
+
+  $rc->skip([
+      __DIR__ . '/src/Database/Traits/IdTrait.php', // @todo: Remove this when moving to attributes for Doctrine
+  ]);
 };
