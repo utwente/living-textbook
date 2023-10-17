b2a78332dc7b8a948c36259326c68b27e3fc4329
Tobias Feijten <tobias@drenso.nl>
2023-10-17 12:14:27 +0200

    Update Doctrine dependencies, update rector and cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,8 +3,9 @@
 return (new PhpCsFixer\Config())
     ->setIndent('  ')
     ->setRules([
-        '@Symfony'                    => true,
-        'binary_operator_spaces'      => [
+        '@Symfony'               => true,
+        'array_indentation'      => true,
+        'binary_operator_spaces' => [
             'default'   => 'align',
             'operators' => [
                 '=>'  => 'align_single_space_minimal',
@@ -26,16 +27,20 @@ return (new PhpCsFixer\Config())
                 '>='  => 'single_space',
             ],
         ],
-        'cast_spaces'                 => ['space' => 'none'],
-        'class_attributes_separation' => ['elements' => ['const' => 'only_if_meta']],
-        'concat_space'                => ['spacing' => 'one'],
-        'increment_style'             => false,
-        'ordered_imports'             => ['imports_order' => ['class', 'function', 'const']],
-        'phpdoc_line_span'            => ['const' => 'single', 'method' => 'single', 'property' => 'single'],
-        'phpdoc_order'                => true,
-        'single_line_throw'           => false,
-        'single_line_comment_spacing' => false,
-        'yoda_style'                  => false,
+        'cast_spaces'                                      => ['space' => 'none'],
+        'class_attributes_separation'                      => ['elements' => ['const' => 'only_if_meta']],
+        'concat_space'                                     => ['spacing' => 'one'],
+        'global_namespace_import'                          => ['import_classes' => true],
+        'increment_style'                                  => false,
+        'method_chaining_indentation'                      => true,
+        'nullable_type_declaration_for_default_null_value' => true,
+        'ordered_imports'                                  => ['imports_order' => ['class', 'function', 'const']],
+        'phpdoc_line_span'                                 => ['const' => 'single', 'method' => 'single', 'property' => 'single'],
+        'phpdoc_order'                                     => true,
+        'phpdoc_to_comment'                                => ['ignored_tags' => ['noinspection', 'noRector']],
+        'single_line_throw'                                => false,
+        'single_line_comment_spacing'                      => false,
+        'yoda_style'                                       => false,
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
--- rector.php
+++ rector.php
@@ -6,11 +6,9 @@ use Rector\Config\RectorConfig;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
 use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Rector\Class_\FormTypeWithDependencyToOptionsRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return static function (RectorConfig $rc): void {
@@ -23,9 +21,7 @@ return static function (RectorConfig $rc): void {
       ReadOnlyPropertyRector::class, // Cannot be used with proxies yet (https://github.com/Ocramius/ProxyManager/issues/737)
       ReturnNeverTypeRector::class, // Not working properly
       ClassPropertyAssignToConstructorPromotionRector::class, // Messes up the annotations
-      TypedPropertyRector::class, // Not usable yet, since the serializer has a bug (https://github.com/schmittjoh/serializer/issues/1282)
       ArraySpreadInsteadOfArrayMergeRector::class, // Not very useful, leads to Phan issue
-      FormTypeWithDependencyToOptionsRector::class, // Broken Rector (https://github.com/rectorphp/rector-symfony/pull/180)
       JsonThrowOnErrorRector::class, // Disable to not change functionality
   ]);
 
