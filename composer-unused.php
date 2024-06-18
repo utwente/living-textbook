3b77e090ed15c3ff74d424cdced93eaa60d7f5a9
Tobias Feijten <tobias@drenso.nl>
2024-06-18 22:27:08 +0200

    Some last fixes

--- composer-unused.php
+++ composer-unused.php
@@ -10,6 +10,8 @@ return static function (Configuration $config): Configuration {
   return $config
       ->addNamedFilter(NamedFilter::fromString('ext-apcu'))
       ->addNamedFilter(NamedFilter::fromString('league/html-to-markdown'))
+      ->addNamedFilter(NamedFilter::fromString('symfony/doctrine-messenger'))
+      ->addNamedFilter(NamedFilter::fromString('symfony/dotenv'))
       ->addNamedFilter(NamedFilter::fromString('symfony/flex'))
       ->addNamedFilter(NamedFilter::fromString('symfony/messenger'))
       ->addNamedFilter(NamedFilter::fromString('symfony/runtime'))
