99d15d0b6a4b563cf33b0fb94501d9f9a5413136
Tobias Feijten <tobias@drenso.nl>
2024-06-25 17:01:05 +0200

    Add Twig CS fixer with default config

---
+++ .twig-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+return (new TwigCsFixer\Config\Config())
+//  ->allowNonFixableRules()
+//  ->setRuleset(
+//    (new TwigCsFixer\Ruleset\Ruleset())
+//      ->addStandard(new TwigCsFixer\Standard\Symfony())
+//      ->overrideRule(new TwigCsFixer\Rules\File\DirectoryNameRule(baseDirectory: 'templates', ignoredSubDirectories: ['bundles', 'components'], optionalPrefix: '_')) // Adapt to allow underscore as prefix
+//      ->addStandard(new TwigCsFixer\Standard\TwigCsFixer())
+//  )
+  ->setFinder(
+    TwigCsFixer\File\Finder::create()
+      ->in(__DIR__ . DIRECTORY_SEPARATOR . 'templates')
+  );
