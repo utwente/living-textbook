c5e2a4f61c015ab19635c38f2eb0280b065a1aba
Tobias Feijten <tobias@drenso.nl>
2024-06-25 17:19:26 +0200

    Fix not automatically fixable issues

--- .twig-cs-fixer.dist.php
+++ .twig-cs-fixer.dist.php
@@ -1,13 +1,13 @@
 <?php
 
 return (new TwigCsFixer\Config\Config())
-//  ->allowNonFixableRules()
-//  ->setRuleset(
-//    (new TwigCsFixer\Ruleset\Ruleset())
-//      ->addStandard(new TwigCsFixer\Standard\Symfony())
-//      ->overrideRule(new TwigCsFixer\Rules\File\DirectoryNameRule(baseDirectory: 'templates', ignoredSubDirectories: ['bundles', 'components'], optionalPrefix: '_')) // Adapt to allow underscore as prefix
-//      ->addStandard(new TwigCsFixer\Standard\TwigCsFixer())
-//  )
+  ->allowNonFixableRules()
+  ->setRuleset(
+    (new TwigCsFixer\Ruleset\Ruleset())
+      ->addStandard(new TwigCsFixer\Standard\Symfony())
+      ->overrideRule(new TwigCsFixer\Rules\File\DirectoryNameRule(baseDirectory: 'templates', ignoredSubDirectories: ['bundles', 'components'], optionalPrefix: '_')) // Adapt to allow underscore as prefix
+      ->addStandard(new TwigCsFixer\Standard\TwigCsFixer())
+  )
   ->setFinder(
     TwigCsFixer\File\Finder::create()
       ->in(__DIR__ . DIRECTORY_SEPARATOR . 'templates')
