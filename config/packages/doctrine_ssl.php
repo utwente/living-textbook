99fe57118ea75b2affa9b97cb1e9ddcbf2f09051
Bob van de Vijver <bob@drenso.nl>
2026-05-16 22:27:06 +0200

    Add configuration option to configure SSL context when needed

---
+++ config/packages/doctrine_ssl.php
@@ -0,0 +1,18 @@
+<?php
+
+use Pdo\Mysql;
+use Symfony\Config\DoctrineConfig;
+
+$connectionOptions = [PDO::ATTR_EMULATE_PREPARES => false];
+if (\filter_var($_ENV['SECURED_DB_CONNECTION'], \FILTER_VALIDATE_BOOL)) {
+  $connectionOptions[Mysql::ATTR_SSL_CA] = '/etc/certs/mysql/ca-cert.pem';
+  $connectionOptions[Mysql::ATTR_SSL_VERIFY_SERVER_CERT] = 1;
+}
+
+return Symfony\Component\DependencyInjection\Loader\Configurator\App::config([
+  'doctrine' => [
+    'dbal' => [
+      'options' => $connectionOptions
+    ]
+  ]
+]);
