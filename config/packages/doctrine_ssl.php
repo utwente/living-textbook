fc8ce6f806f9cf6ef8704f69fb4686154f83e0ee
Bob van de Vijver <bob@drenso.nl>
2026-05-19 18:20:32 +0200

    Make database connection fully configurable

--- config/packages/doctrine_ssl.php
+++ config/packages/doctrine_ssl.php
@@ -2,13 +2,19 @@
 
 use Pdo\Mysql;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+
 $connectionOptions = [PDO::ATTR_EMULATE_PREPARES => false];
-if (\filter_var($_ENV['SECURED_DB_CONNECTION'], \FILTER_VALIDATE_BOOL)) {
-  $connectionOptions[Mysql::ATTR_SSL_CA]                 = '/etc/certs/mysql/ca-cert.pem';
-  $connectionOptions[Mysql::ATTR_SSL_VERIFY_SERVER_CERT] = 1;
+if (\filter_var($_ENV['SECURED_DB_ENABLED'] ?? false, \FILTER_VALIDATE_BOOL)) {
+  $connectionOptions[Mysql::ATTR_SSL_CA]                 = env('SECURED_DB_SSL_CA');
+  $connectionOptions[Mysql::ATTR_SSL_VERIFY_SERVER_CERT] = env('SECURED_DB_SSL_VERIFY_SERVER_CERT');
 }
 
 return Symfony\Component\DependencyInjection\Loader\Configurator\App::config([
+  'parameters' => [
+    'env(SECURED_DB_SSL_CA)'                 => '/etc/ssl/certs/ca-certificates.crt',
+    'env(SECURED_DB_SSL_VERIFY_SERVER_CERT)' => 'true',
+  ],
   'doctrine' => [
     'dbal' => [
       'options' => $connectionOptions,
